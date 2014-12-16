fced1632fd3da45a159624f1aaee981285bd6ab9
Thomas Zou <thomas.zyc@hotmail.com>
2014-12-17 00:38:47 +0800

    comment

--- src/ui/ColorStyle.java
+++ src/ui/ColorStyle.java
@@ -2,6 +2,6 @@ package ui;
 
 public class ColorStyle {
 
-	public static String DARK_BLACK = "-fx-background-color: #A8A8A8";
+	public static String DARK_BLACK = "-fx-background-color: #A8A8A8"; //background
 	
 }
