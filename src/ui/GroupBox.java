66c15a560efaa3ca44967a8398d1532f4582b951
Thomas Zou <thomas.zyc@hotmail.com>
2014-12-23 18:23:25 +0800

    commit

--- src/ui/GroupBox.java
+++ src/ui/GroupBox.java
@@ -1,15 +1,25 @@
 package ui;
 
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
 public class GroupBox {
 	
+	public VBox vbox = new VBox();
+	public Label label = new Label();
+	public String group;
 	
 	
-	public GroupBox() {
+	public GroupBox(String str) {
+		group = str;
+		label.setText("Group " + str);
+		vbox.getChildren().add(label);
 		
 	}
 	
-	public void build() {
+	public void insert() {
 		
 	}
+	
 
 }
